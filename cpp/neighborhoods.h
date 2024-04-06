d056cb7183aec58d6203b39c5ed8c322c7586ec3
timarque <56795474+timarque@users.noreply.github.com>
2024-04-06 08:04:19 -0700

    Delete cpp directory

--- cpp/neighborhoods.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NO_H_
-#define _NO_H_
-
-
-long *exchange(long int *solution, int i, int j);
-long *transpose(long int *solution, int i, int j);
-long *insert(long int *solution, int i, int j);
-
-#endif
